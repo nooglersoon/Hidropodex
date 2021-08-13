97c5f0319bb0adc78d64f00abd103df5a5a79dc3
Fauzi Achmad Bangsa Diria <nooglersoon@outlook.co.id>
2021-08-13 15:55:44 +0700

    Initial Commit

---
+++ Hidropodex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hidropodex
+//
+//  Created by Fauzi Achmad B D on 13/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hidropodex/HidropodexApp.swift
@@ -0,0 +1,17 @@
+//
+//  HidropodexApp.swift
+//  Hidropodex
+//
+//  Created by Fauzi Achmad B D on 13/08/21.
+//
+
+import SwiftUI
+
+@main
+struct HidropodexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
